e5cf8af87dc268584e0a0e240288240afaf9fe4a
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-27 23:34:29 +0100

    feat: apply Merriweather Sans font to header title

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="w-full py-5 border-b-1 border-violet-600 text-black mb-5">
           <div className="max-w-4xl mx-auto px-4 flex items-center justify-between">
             <div>
-              <h1 className="text-2xl md:text-3xl font-bold tracking-wide">
+              <h1 className="text-2xl text-gray-900 md:text-3xl font-bold tracking-wide font-[Merriweather_Sans]">
                 Finelyia
               </h1>
               <p className="text-sm text-gray-600 md:text-base mt-2 font-light italic">
@@ -14,7 +14,7 @@ export default function Header() {
               <button className="font-semibold hover:underline">
                 Se connecter
               </button>
-              <button className="text-black font-semibold border border-violet-600 px-3 py-1 rounded hover:bg-violet-600 hover:text-gray-50">
+              <button className="text-black font-semibold border border-violet-600 px-3 py-1 rounded hover:bg-violet-600 duration-200 hover:text-gray-50">
                 S'inscrire
               </button>
             </div>
