3386969d448aab50def17c0786f0613a00b475b2
Thomas OvO <thonnard.thomas@gmail.com>
2025-11-01 18:48:57 +0100

    chore: add initial firebase.js config file

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase configuration
+const firebaseConfig = {
+  //...
+};
+
+const app = initializeApp(firebaseConfig);
