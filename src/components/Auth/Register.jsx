724f9378a43247c949a32946d6d943f61d3d1a20
Thomas OvO <thonnard.thomas@gmail.com>
2025-11-16 23:38:40 +0100

    feat(Register): improve form design with logo and shadow

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { ClipLoader } from "react-spinners";
+import logo from "../../assets/Logo_Finelyia1.png"
 
 export default function Register() {
   const [email, setEmail] = useState("");
@@ -59,11 +60,14 @@ export default function Register() {
   };
 
   return (
-    <div className="flex h-screen justify-center items-center bg-[#fafbfe]">
+    <div className="flex h-screen justify-center items-center bg-[#fafbfe] m-6 mx-auto">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col border-gray-400 rounded-lg p-6 shadow-gray-300 shadow-lg mx-auto max-w-sm"
+        className="flex flex-col border-gray-400 rounded-lg px p-6 shadow-gray-300 shadow-lg mx-auto max-w-md"
       >
+        <div className="w-20 h-20 md:w-32 md:h-32 mx-auto mb-5">
+        <img src={logo} alt="logo" className="shadow-gray-300 shadow-lg rounded-lg" />
+      </div>
         <h2 className="text-center mb-5 text-xl md:text-2xl font-semibold">
           Créer un compte
         </h2>
