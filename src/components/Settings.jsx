c4edb06d46f23c7e052e20c78d3ec29482b57a04
Thomas OvO <thonnard.thomas@gmail.com>
2025-11-22 00:42:02 +0100

    feat(Settings): add toggles and language select to settings page

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,13 +3,32 @@ export default function Paramètres() {
     <div className="flex flex-col w-1/2 mx-auto">
       <h2 className="font-semibold text-3xl text-center mb-4">Paramètres</h2>
       <div className="border-2 border-gray-300 rounded-xl bg-white p-4">
-        <div className="flex items-center gap-3">
+        <div className="flex items-center justify-between gap-3 border-b border-b-gray-300 py-2">
           <span className="text-lg">Mode sombre</span>
           <label className="relative inline-flex items-center cursor-pointer">
             <input type="checkbox" className="sr-only peer" />
             <div className="w-11 h-6 bg-gray-200 peer-checked:bg-violet-600 rounded-full peer peer-focus:ring-2 peer-focus:ring-violet-500 transition-all"></div>
             <div className="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-all peer-checked:translate-x-5"></div>
           </label>
+        </div>
+
+        <div className="flex items-center justify-between gap-3 border-b border-b-gray-300 py-2">
+          <span className="text-lg">Notifications</span>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" className="sr-only peer" />
+            <div className="w-11 h-6 bg-gray-200 peer-checked:bg-violet-600 rounded-full peer peer-focus:ring-2 peer-focus:ring-violet-500 transition-all"></div>
+            <div className="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-all peer-checked:translate-x-5"></div>
+          </label>
+        </div>
+
+        <div className="flex items-center justify-between gap-3 border-b border-b-gray-300 py-2">
+          <label className="relative inline-flex items-center cursor-pointer">
+            Langue
+          </label>
+          <select>
+            <option value="francais">Français</option>
+            <option value="francais">Anglais</option>
+          </select>
         </div>
       </div>
     </div>
