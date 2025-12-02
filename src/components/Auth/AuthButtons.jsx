f3922c4791fa9f5dcf6912200f9520f34335d1a4
Thomas OvO <thonnard.thomas@gmail.com>
2025-12-02 17:13:59 +0100

    feat(AuthButtons): translation

--- src/components/Auth/AuthButtons.jsx
+++ src/components/Auth/AuthButtons.jsx
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function AuthButtons() {
+  const { t } = useTranslation();
   return (
     <div className="flex items-center md:gap-3">
       <Link
         to="/login"
         className="font-semibold hover:text-violet-600 duration-200 px-2 py-1 text-sm md:text-base min-w-[90px] max-w-[120px] whitespace-nowrap dark:text-white"
       >
-        Se connecter
+        {t("authbuttons.login")}
       </Link>
       <Link
         to="/register"
         className="text-black font-semibold border border-violet-600 text-sm px-3 py-1 rounded hover:bg-violet-600 duration-200 hover:text-gray-50 md:text-base min-w-[90px] max-w-[120px] whitespace-nowrap dark:text-white"
       >
-        S'inscrire
+        {t("authbuttons.register")}
       </Link>
     </div>
   );
